0546c52dd7fd99c5810e0d1733b91dc88cbd7022
Gushihiro <robinsonhiroto@gmail.com>
2021-08-06 21:06:52 -0700

    set up CRA, pulled data from API, basic data rendered on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+//https://avb-contacts-api.herokuapp.com
+
+export type UserDataType = {
+  id: number,
+  firstName: string,
+  lastName: string,
+}
+
 
 function App() {
+
+  const [userData, setUserData] = useState([] as UserDataType[])
+
+  const getContacts = async (): Promise<void> => {
+    const pullData = await axios.get('https://avb-contacts-api.herokuapp.com/contacts/paginated')
+    const userInfo:UserDataType[] = pullData.data.contacts
+    try {
+      console.log(userInfo)
+      await setUserData(userInfo)
+    } catch(err) {
+      console.log(err)
+    }
+    
+  }
+
+  useEffect(() => {
+    getContacts()
+    
+  }, []) 
+
+  console.log(userData)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contacts-CRUD</h1>
+      {userData.map((user: UserDataType) => (
+          <h2 key={user.id}>Name: {user.firstName} {user.lastName}</h2>
+        ))}
     </div>
   );
 }
